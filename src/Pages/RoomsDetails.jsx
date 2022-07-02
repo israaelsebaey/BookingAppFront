556c16a47ad66efe7a2856286ee889f0411984dc
israaelsebaey <israaalsebaei30@gmail.com>
2022-07-02 12:21:34 +0200

    Add Rooms Details Page

---
+++ src/Pages/RoomsDetails.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import SecHeader from '../Components/SecondHeader/SecondHeader';
+import Details from '../Components/RoomsDetails/Details';
+
+function RoomsDetails(){
+    return(
+        <Fragment>
+            <SecHeader/>
+            <Details/>
+        </Fragment>
+    )
+}
+
+export default RoomsDetails;
